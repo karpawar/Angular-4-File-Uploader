504061a6f73a49fbd6c4eb0fbacfb4153464a9fa
karpawar <karpawar@gmail.com>
2017-09-28 00:40:48 +0530

    md-md-accent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MatToolbarModule } from '@angular/material';
-import { MatCardModule, MatTableModule, MdButtonModule, MatInputModule, MdCheckboxModule } from '@angular/material';
+import { MatCardModule, MatTableModule, MatExpansionModule, MdButtonModule, MatInputModule, MdCheckboxModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
@@ -13,7 +13,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppComponent
   ],
   imports: [
-    BrowserModule, BrowserAnimationsModule, MatTableModule, MatInputModule, MatToolbarModule, MdButtonModule, MdCheckboxModule, MatCardModule
+    BrowserModule, BrowserAnimationsModule, MatExpansionModule, MatTableModule, MatInputModule, MatToolbarModule, MdButtonModule, MdCheckboxModule, MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
