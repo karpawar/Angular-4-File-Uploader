22baaa777aa2bae6b1870d0e46b47b71229b6735
karpawar <karpawar@gmail.com>
2017-09-28 00:24:44 +0530

    Commit message

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material';
+import { MatCardModule, MatTableModule, MdButtonModule, MatInputModule, MdCheckboxModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, BrowserAnimationsModule, MatTableModule, MatInputModule, MatToolbarModule, MdButtonModule, MdCheckboxModule, MatCardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
